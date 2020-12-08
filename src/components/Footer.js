251bb5739202af92cb776a1c550b692a36091844
Matt Austin <mpaustin13@gmail.com>
2020-12-07 19:38:36 -0500

    changing footer icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { connect } from 'react-redux'
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import TwitterIcon from '@material-ui/icons/Twitter';
+import FitnessCenter from '@material-ui/icons/FitnessCenter';
 
 export const Footer = () => {
     return (
         <div>
             <BottomNavigation>
-                <BottomNavigationAction label='test' icon={<TwitterIcon/>} />
+                <BottomNavigationAction label='test' icon={<FitnessCenter/>} />
             </BottomNavigation>
         </div>
     )
