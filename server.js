933905cf4886bf259b77f55bf9c5e861b247101b
Matt Austin <mpaustin13@gmail.com>
2020-12-09 18:25:14 -0500

    /login endpoint in server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = 3001;
 // console.log('process env', process.env);
 
 const mw = (req, res, next) => {
-    console.log('middleware', req.url);
+    console.log('req.url', req.url);
     next();
 }
 
@@ -39,6 +39,34 @@ app.get('/workouts/:user', mw, async (req, res) => {
     res.status(200).send(values.rows);
 })
 
+app.post('/login', mw, async (req, res) => {
+
+    const auth = req.headers.authorization;
+
+    if (auth && auth.startsWith('Basic')) {
+
+        const encoded = auth.substring('Basic '.length).trim();
+        const decoded = Buffer.from(encoded, 'base64').toString('binary');
+
+        const index = decoded.indexOf(':');
+        const username = decoded.substring(0,index);
+        const password = decoded.substring(index + 1);
+
+        console.log('username', username);
+        console.log('password', password);
+
+        const user = await pgClient.query(
+            'select * from trainusers where username = $1',
+            [username]
+        )
+        console.log('user.rows[0]', user.rows[0])
+        if (!user || !user.rows || user.rows[0].password !== password) {
+            res.status(401).send();
+        }
+        res.status(200).send();
+    }
+    res.status(403).send();
+}); 
 
 app.listen(port, () => {
     console.log(`Train Tracker listening on port ${port}`);
