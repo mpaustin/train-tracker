ebb68c0e58177dd06cee46237fc3191f2942b21d
Matt Austin <mpaustin13@gmail.com>
2020-12-26 12:59:11 -0500

    adding server endpoint for adding new workout

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3001;
 
 // console.log('process env', process.env);
@@ -42,7 +45,27 @@ app.get('/workouts', mw, async (req, res) => {
 })
 
 app.post('/workouts/new', mw, async (req, res) => {
-    // TODO: build out endpoint for adding new workout based on user
+
+    const user = req.body.user;
+    const date = req.body.date;
+    const type = req.body.type;
+    const description = req.body.description;
+    const meditation = req.body.meditation;
+    const sauna = req.body.sauna;
+
+    await pgClient.query(
+        'insert into trainworkouts (username,wdate,type,description,meditation,sauna)' +
+        'values ($1,$2,$3,$4,$5,$6)',
+        [user,date,type,description,meditation,sauna],
+        (err, res) => {
+            if (err) {
+                console.log('ERROR', err);
+                res.status(500).send();
+            }
+        }
+    );
+    console.log('SUCCESS', res);
+    res.status(200).send();
 })
 
 app.get('/login', mw, async (req, res) => {
