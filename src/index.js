cc4071d4510dabec6e68a669276cbf28d6c10c25
Matt Austin <mpaustin13@gmail.com>
2021-01-05 15:39:28 -0500

    adding auth0 provider

--- src/index.js
+++ src/index.js
@@ -7,19 +7,26 @@ import store from './redux/index';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const theme = createMuiTheme();
 
 ReactDOM.render(
-  <ThemeProvider theme={theme} >
-    <Provider store={store}>
-      <React.StrictMode>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </React.StrictMode>
-    </Provider>
-  </ThemeProvider>,
+  <Auth0Provider
+    domain="mpaustin.us.auth0.com"
+    clientId="UjDu5lV6rCJaL6Ss7sOwLZbkyI4TdFO1"
+    redirectUri={window.location.origin}
+  >
+    <ThemeProvider theme={theme} >
+      <Provider store={store}>
+        <React.StrictMode>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </React.StrictMode>
+      </Provider>
+    </ThemeProvider>
+  </Auth0Provider>,
   document.getElementById('root')
 );
 
