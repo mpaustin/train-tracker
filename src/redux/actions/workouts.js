401cfac6297ac7157f971db82540bbe6ca74d842
Matt Austin <mpaustin13@gmail.com>
2021-01-08 19:44:30 -0500

    adding api token validation for GET /workouts

--- src/redux/actions/workouts.js
+++ src/redux/actions/workouts.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useAuth0 } from '@auth0/auth0-react';
 
 export const GET_WORKOUTS = {
     request: 'GET_WORKOUTS_REQUEST',
@@ -12,12 +13,24 @@ export const ADD_WORKOUT = {
     error: 'ADD_WORKOUT_ERROR',
 }
 
-export const getWorkouts = (user) => async (dispatch) => {
+export const getWorkouts = (user, token) => async (dispatch) => {
+
+    // const { getAccessTokenSilently } = useAuth0();
+
+    // const token = await getAccessTokenSilently({
+    //     audience: "https://mpaustin.us.auth0.com/api/v2/",
+    //     scope: "read:workouts"
+    // })
+
     dispatch({ type: GET_WORKOUTS.request });
+
     axios.get('http://localhost:3001/workouts', {
         params: {
             user,
         },
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
     }).then(res => {
         dispatch({ type: GET_WORKOUTS.success, payload: res.data });
     }).catch(error => {
