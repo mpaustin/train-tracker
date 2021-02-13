badea7fdea8084d2c657b7dba468f2343bf72d91
Matt Austin <mpaustin13@gmail.com>
2021-02-13 16:58:40 -0500

    testing removing server endpoints

--- server.js
+++ server.js
@@ -9,17 +9,17 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, './build')))
 
-app.get('/', function(_, res) {
-  res.sendFile(path.join(__dirname, './build/index.html'), function(err) {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
-})
+// app.get('/', function(_, res) {
+//   res.sendFile(path.join(__dirname, './build/index.html'), function(err) {
+//     if (err) {
+//       res.status(500).send(err)
+//     }
+//   })
+// })
 
-app.get('/.well-known/acme-challenge/:content', function(req, res) {
-  res.send('Kwpj3lMeT3b33LvUVCzB7cmPI_YWEbuSNaxTVzqQgCE.j_v_L8rrcyRQmuz522XRsY32cG0YFQz3QyeQ8nP2XEI')
-})
+// app.get('/.well-known/acme-challenge/:content', function(req, res) {
+//   res.send('Kwpj3lMeT3b33LvUVCzB7cmPI_YWEbuSNaxTVzqQgCE.j_v_L8rrcyRQmuz522XRsY32cG0YFQz3QyeQ8nP2XEI')
+// })
 
 app.listen(port, () => {
     console.log(`Train Tracker (React) listening on port ${port}`);
