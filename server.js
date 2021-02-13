144511c0c2fffef476a142f8cb4e932c0d2b2882
Matt Austin <mpaustin13@gmail.com>
2021-02-13 17:37:30 -0500

    testing addition of node server.js to start script with sls token

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, './build')))
+// app.use(express.static(path.join(__dirname, './build')))
 
 // app.get('/', function(_, res) {
 //   res.sendFile(path.join(__dirname, './build/index.html'), function(err) {
@@ -17,9 +17,9 @@ app.use(express.static(path.join(__dirname, './build')))
 //   })
 // })
 
-// app.get('/.well-known/acme-challenge/:content', function(req, res) {
-//   res.send('Kwpj3lMeT3b33LvUVCzB7cmPI_YWEbuSNaxTVzqQgCE.j_v_L8rrcyRQmuz522XRsY32cG0YFQz3QyeQ8nP2XEI')
-// })
+app.get('/.well-known/acme-challenge/:content', function(req, res) {
+  res.send('sJgyqjxi1WZ9DLcM3bOXoC6UCOL1iyYedkEESERRfqs.j_v_L8rrcyRQmuz522XRsY32cG0YFQz3QyeQ8nP2XEI')
+})
 
 app.listen(port, () => {
     console.log(`Train Tracker (React) listening on port ${port}`);
